e9313e0bffaac1aefbca6dac216bae2512b5f5cd
soutaschool <55971123+soutaschool@users.noreply.github.com>
2019-12-06 12:20:13 +0900

    Add files via upload

---
+++ 新規プロフェクト/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $('#first-text').html('<h1>辻堂海浜公園のフロントページ</h1>');
+    $('#first-text').css({
+    	'color': 'red'
+    });
+    $('#second-text').html('<h2>フロントページです</h2>');
+    $('#second-text').css({
+    	'color': 'black',
+      'border-bottom': '1px solid'
+    });
+
+
+    $('#3-text').html('辻堂海浜公園とは？');
+    $('#3-text').css({
+      'font-size': '23px',
+    	'color': 'black'
+    });
+
+    $('#4').html('辻堂海浜公園は様々な施設があり、豊かな自然に囲まれた場所です。<br>特にジャンボプールが有名で、<br>公園の中央に位置する北口から、芝生公園、砂場、交通公園、多目的広場などがあり<br>とても自然に囲まれています。');
+    $('#4').css({
+    	'color': 'black'
+    });
+
+    $('#f-p').css({
+    	'background-image': 'url(images/1.jpg)',
+      'background-size': 'cover',
+      'background-position': 'center',
+      'width': '70%',
+      'height': '500px'
+    });
+
+    $()
+
+
+
+
+
+
+
+    var name = ["辻堂海浜公園のフロントページ","辻堂海浜公園とは？","",""]
+});
